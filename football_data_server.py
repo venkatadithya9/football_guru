74b5ee71cb39b8a818c0197084c9fea285530f70
Venkat Adithya Amula <venkatadithya9@gmail.com>
2026-09-16 23:31:30 +0530

    Exploring other APIs, and fastMCP instead of legacy MCP package

--- football_data_server.py
+++ football_data_server.py
@@ -13,6 +13,7 @@ from langgraph.managed.is_last_step import RemainingSteps
 
 from langchain.mcp import MCPAdapter
 from mcp.server import MCPServer
+import httpx2
 
 class InputState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
@@ -34,12 +35,55 @@ LEAGUES = {"pl": 2021,
              "ligue1": 2015,
               "seria": 2019 }
 
+# ---------- HELPER FUNCTIONS -----------------
+
+async def make_request(url: str) -> dict:
+    """
+    Make an asynchronous HTTP GET request to the specified URL with the provided headers.
+
+    Args:
+        url (str): The URL to send the GET request to.
+        headers (dict): A dictionary of HTTP headers to include in the request.
+
+    Returns:
+        dict: The JSON response from the server.
+
+    Raises:
+        httpx.HTTPStatusError: If the response status code indicates an error.
+    """
+
+    headers = {"X-Auth-Token": FOOTBALL_DATA_API_KEY}
+
+    async with httpx2.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    return None
+
 # --------- MCP DEFINITION --------------
 
 mcp = MCPServer("football_data_server")
 
+@mcp.tool()
+async def get_league_standings(league: str) -> str:
+    """
+    Get the standings for a given league.
 
+    Args:
+        league (str): The name of the league (e.g., "pl" for Premier League, "laliga" for La Liga, "bundesliga" for Bundesliga, "ligue1" for Ligue 1, "seria" for Serie A).
+    
+    """
+    league_id = LEAGUES.get(league.lower())
+    if not league_id:
+        raise ValueError(f"League '{league}' not found.")
+    
+    url = f"{BASE_ADDRESS}competitions/{league_id}/standings"
+    data = await make_request(url)
 
+    if not data or "standings" not in data:
+        return "No standings data could be retrieved."
+    
 
 # async def main():
 #     async with MCPAdapter("https://football.com/mcp") as adapter:
